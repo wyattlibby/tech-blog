216637686dd4d253b284658e2a1de4b46a88e462
Brad <kingdomlandscapevt@gmail.com>
2022-01-20 14:17:06 -0500

    added more dependencies and settings

--- server.js
+++ server.js
@@ -3,5 +3,17 @@ const session = require("express-session");
 const express_handlebars = require("express-handlebars");
 const path = require("path");
 require("dotenv").config();
-
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const app = express();
+const port = process.env.PORT || 3001;
+const sess = {
+    secret: "i have a secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
