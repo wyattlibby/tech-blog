852da26481d940534e923972958aec3252c40d1b
Brad <kingdomlandscapevt@gmail.com>
2022-01-20 14:09:13 -0500

    added dependencies to server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const session = require("express-session");
+const express_handlebars = require("express-handlebars");
+const path = require("path");
+require("dotenv").config();
+
+
